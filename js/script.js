ce94a9dcd0a3c6e621063d8e010f435a0c7fc4da
leonardojr1210 <leleo.ribeiri.spiin@gmail.com>
2023-11-17 18:49:51 -0300

    adicionando modal

--- js/script.js
+++ js/script.js
@@ -14,5 +14,42 @@ function calcularIMC(event) {
   // Arredondando o IMC para duas casas decimais
   imc = imc.toFixed(2);
 
+  modalImc(+imc);
+
   return (resultado.value = imc);
+}
+
+function modalImc(imc) {
+  const modal = document.querySelector("#modal");
+  const btnClose = document.querySelector("#closeModal");
+  const textModal = document.querySelector("#textModal");
+
+  switch (true) {
+    case imc >= 18 && imc <= 25:
+      textModal.innerHTML = `Seu IMC é de: <strong class="text-success">${imc}</strong>. Você está no Peso`;
+      break;
+    case imc >= 26 && imc <= 30:
+      textModal.innerHTML = `Seu IMC é de: <strong class="text-warning">${imc}</strong>. Você está com sobrepeso, indicamos que você procure um médico.`;
+      break;
+    case imc >= 31 && imc <= 41:
+      textModal.innerHTML = `Seu IMC é de: <strong class="text-danger">${imc}</strong>. Você está com obesidade, indicamos que você procure um médico.`;
+      break;
+    case imc >= 41:
+      textModal.innerHTML = `Seu IMC é de: <strong class="text-danger">${imc}</strong>. Você está com obesidade morbida, indicamos que você procure um médico.`;
+      break;
+    default:
+      null;
+  }
+
+  modal.style.cssText = `
+    display: block; 
+    opacity: 1;
+  `;
+
+  btnClose.addEventListener("click", () => {
+    modal.style.cssText = `
+      display: none; 
+      opacity: 0;
+    `;
+  });
 }
